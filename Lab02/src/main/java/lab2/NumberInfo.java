c2caaa1e529537de3291318b735e779f93a55b91
br00419 <br00419@heron086.eps.surrey.ac.uk>
2022-10-04 11:16:15 +0100

    working on lab02

---
+++ Lab02/src/main/java/lab2/NumberInfo.java
@@ -0,0 +1,19 @@
+package lab2;
+
+public class NumberInfo {
+	private int limit;
+	private int value;
+
+	public NumberInfo(int limit) {
+		super();
+		value = 0;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
---
+++ Lab02/src/main/java/lab2/NumberTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package lab2;
+
+/**
+ * @author br00419
+ *
+ */
+public class NumberTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		NumberInfo number = new NumberInfo(3);
+		System.out.println(number.getValue());
+
+		number.getValue();
+		System.out.println(number.getValue());
+
+	}
+
+}
+
