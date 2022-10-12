9914f357eae888bac403a86d8e975b2ca8f5d215
bipinrai <bipinrai@localhost>
2022-10-12 22:54:36 +0100

    lab2exr2

---
+++ Lab02/src/main/java/exercise2/Meal.java
@@ -0,0 +1,24 @@
+package exercise2;
+
+public class Meal {
+	
+	private Food maincourse;
+	private Food dessert;
+	
+	public Meal(Food maincourse, Food dessert) {
+			super();
+			this.maincourse = maincourse;
+			this.dessert = dessert;
+	}
+
+	public double calculateTotalSugarLevel() {
+		
+		return (maincourse.getAmountOfSugar()+ dessert.getAmountOfSugar());
+		
+		
+	}
+	public String toString() {
+		return "Meal [mainCourse = " + maincourse.getName() + ", dessert = " + dessert.getName() + "]";
+		
+	}
+}
