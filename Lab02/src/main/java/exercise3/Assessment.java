3ab13f38de65d39acc69902b89f9d671401b6e32
bipinrai <bipinrai@localhost>
2022-10-18 11:45:23 +0100

    ex.3 lab 2

---
+++ Lab02/src/main/java/exercise3/Assessment.java
@@ -0,0 +1,17 @@
+package exercise3;
+
+public class Assessment {
+	
+	private String name;
+	private int maximum;
+	private double mark;
+	
+	public String getName()  {
+		return name;
+	
+	}
+
+	public int getMaximum() {
+		return maximum;
+		
+	}
