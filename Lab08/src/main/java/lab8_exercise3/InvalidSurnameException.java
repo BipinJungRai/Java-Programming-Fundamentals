fa7e297463fd1da608de149c70b683d77d9b1415
bipinrai <bipinrai@bipins-mbp>
2022-12-28 22:40:36 +0000

    Lab 8 done

---
+++ Lab08/src/main/java/lab8_exercise3/InvalidSurnameException.java
@@ -0,0 +1,17 @@
+package lab8_exercise3;
+
+public class InvalidSurnameException extends Exception {
+
+	private static final long serialVersionUID = -6871978051115433827L;
+	
+	public InvalidSurnameException() {
+		super("Invalid Surname");
+	}
+
+	public InvalidSurnameException(String message) {
+		super(message);
+	}
+
+}
+
+
