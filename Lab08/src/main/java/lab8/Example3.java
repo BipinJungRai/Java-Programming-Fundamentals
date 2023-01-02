a3c533a1fdc809fbb5296d5641b9206b194f9f6f
bipinrai <bipinrai@bipins-mbp>
2023-01-02 19:05:47 +0000

    DONZO.

--- Lab08/src/main/java/lab8/Example3.java
+++ Lab08/src/main/java/lab8/Example3.java
@@ -1,16 +1,17 @@
 package lab8;
-
+ 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-
+ 
 /**
  * A simple class used to test out IOExceptions.
  * 
  * @author Stella Kazamia
  */
 public class Example3 {
-
+ 
 	/**
 	 * Main entry point: this is the first method that is run.
 	 * 
@@ -19,8 +20,23 @@ public class Example3 {
 	public static void main(String[] args) {
 		File file = new File("test1.txt");
 		FileWriter fileWriter = null;
-
-		fileWriter = new FileWriter(file);
-
+ 
+		try {
+			fileWriter = new FileWriter(file);
+			fileWriter.write("Adding this line to the file");
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not find file " + file);
+		} catch (IOException e) {
+			System.out.println("Something went wrong");
+		} finally {
+			if (fileWriter != null) {
+				try {
+					fileWriter.close();
+				} catch (IOException e) {
+					System.out.println("Could not close file");
+				}
+			}
+		}
 	}
 }
+ 
