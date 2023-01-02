84875189401469972c400750970bd99abb929eb9
Rai, Bipindra (UG - Computer Science) <br00419@surrey.ac.uk>
2023-01-02 19:30:30 +0000

    done???

--- Lab09/src/main/java/lab9_exercise3/Mall.java
+++ Lab09/src/main/java/lab9_exercise3/Mall.java
@@ -1,16 +1,16 @@
 package lab9_exercise3;
-
+ 
 import java.util.HashMap;
 import java.util.Map;
-
+ 
 public class Mall extends Shop {
 	private HashMap<Integer, Shop> shops;
-
+ 
 	public Mall() {
 		super();
 		shops = new HashMap<Integer, Shop>();
 	}
-
+ 
 	public void addShop(int number, Shop shop) {
 		if (shop == null) {
 			throw new NullPointerException();
@@ -20,10 +20,10 @@ public class Mall extends Shop {
 		}
 		shops.put(number, shop);
 	}
-
+ 
 	public String displayAllShops() {
 		StringBuffer display = new StringBuffer();
-
+ 
 		for (Map.Entry<Integer, Shop> shop : shops.entrySet()) {
 			display.append(
 					shop.getKey() + " : " + shop.getValue().displayCatalogue());
